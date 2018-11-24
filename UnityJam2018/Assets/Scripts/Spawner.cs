c7b8eb0fe4e39a3edc7538a7bd0bbb6932dbc451
antonin boucarut <antoninbou@hotmail.com>
2018-11-24 17:46:02 -0500

    aleatoire spawner

--- UnityJam2018/Assets/Scripts/Spawner.cs
+++ UnityJam2018/Assets/Scripts/Spawner.cs
@@ -34,31 +34,38 @@ public class Spawner : MonoBehaviour {
     */
 
     
-
-    public GameObject[] movingObjectsSpawnPoints;
+    //Liste des transform pour les spawns
+    public Transform [] movingObjectsSpawnPoints;
     public Transform plateformTransform;
 
+    //Prefab de la plateform
     public GameObject plateforme;
 
     private List<GameObject> biomeObstacles = new List<GameObject>();
     private Material spawningMaterial;
 
+
+    //Nombre de plateforme de transition
     public int transitionPlateformNb;
 
+
+    //File paths
     public string filePathSnow;
     public string filePathPlain;
     public string filePathDesert;
 
- 
-
-
+    /*Gains pour le spawn*/
+    public int maxModuloValue;
+    private int primaryValue;
+    private int position;
+    private int lastObjectSize;
 
     // Use this for initialization
     void Start ()
     {
 
-
-
+        position = 0;
+        Random.InitState(Mathf.FloorToInt(Time.deltaTime * 1999));
         OnBiomeChange();
        
     }
@@ -74,7 +81,7 @@ public class Spawner : MonoBehaviour {
 
 
 
-
+    //Change le materiel actuel et la liste du prefab en fonction du biome
     public void OnBiomeChange()
     {
         string path = "";
@@ -118,6 +125,19 @@ public class Spawner : MonoBehaviour {
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+    //Gere le spawn on collider exit
     private void OnTriggerExit(Collider other)
     {
         if (other.tag.Contains("Obstacles"))
@@ -130,15 +150,74 @@ public class Spawner : MonoBehaviour {
 
     private void MovingObjectSpawn()
     {
+        
+        //Tant qu'il y a de la place sur la plateforme
+        while (position < 5)
+        { 
+            //Primary value corresponds a la variable aleatoire
+
+            primaryValue =  Random.Range(0, maxModuloValue);
+            GameObject g = ObjectToSpawn(primaryValue);
+            Transform t= movingObjectsSpawnPoints[position];
+
+            //Verifier qu'on spawn un vrai objet
+            if (g)
+            {   //Pour gerer les differents ofset de position sur la plateforme, offset de 1 quand cest un objet de taille 3 
+                if (lastObjectSize == 3)
+                    t = movingObjectsSpawnPoints[position + 1];
+                else if (lastObjectSize == 2) //Faire la moyene des deux points si c'est un objet de taille 2
+                    t.SetPositionAndRotation(new Vector3(((movingObjectsSpawnPoints[position].position.x + movingObjectsSpawnPoints[position+1].position.x) /2), movingObjectsSpawnPoints[position].position.y, movingObjectsSpawnPoints[position].position.z), movingObjectsSpawnPoints[position].rotation);
+                Instantiate(g,t);
+            }
 
 
-
-
+        }
 
 
 
     }
 
+    //Permet de generer a partir du nombre aleatoire l'objet a faire apparaitre
+    private GameObject ObjectToSpawn(int value)
+    {
+
+        //Retourne un objet de taille 1 
+        if (value <= maxModuloValue - 7 - position)
+        {
+            position++;
+            lastObjectSize = 1;
+            return biomeObstacles[0];
+
+        }
+        else if (value <= maxModuloValue - 5 - position)
+        {   //Retourne un objet de taille 2
+            position += 2;
+            lastObjectSize = 2;
+            return biomeObstacles[1];
+
+        }
+        else if (value <= maxModuloValue - 3 - position)
+        {
+            //Retourne un objet de taille 3
+            position += 3;
+            lastObjectSize = 3;
+            return biomeObstacles[2];
+        }
+        else
+        {
+            //Retourne un objet vide de taille 1
+            lastObjectSize = 1;
+            position++;
+            return null;
+        }
+        
+    
+    } 
+
+
+
+
+    //Spawn des plateformes
     private void PlateformSpawn()
     {
         GameObject g = Instantiate(plateforme, plateformTransform);
